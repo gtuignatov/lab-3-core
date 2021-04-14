f38fadc62cc7fc463559aa4ac4d03f4069a09df6
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-15 00:39:05 +0600

    Lab 6 - ok!
    1. Поля класса лучше не использовать для всех переменных
    1.1. Если переменную можно сделать локальной - лучше сделать. т.к. это упрощает понимание.
    2. Упростил компаратор
    3. Скастил один из int до long иначе использование long лишено смысла

--- src/ru/luxoft/courses/lab6/CheckoutList.java
+++ src/ru/luxoft/courses/lab6/CheckoutList.java
@@ -1,28 +1,23 @@
 package ru.luxoft.courses.lab6;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class CheckoutList {
 
-    private int maxProducts;
-    private String[] arrayOfProducts;
-    private Integer[] arrayOfCosts;
-    private Integer[] arrayOfCounts;
-    private String[] sortedProducts;
+    private final int maxProducts;
+    private final String[] arrayOfProducts;
+    private final Integer[] arrayOfCosts;
+    private final Integer[] arrayOfCounts;
     private int count;
-    private String s;
-    private String[] parts;
-    private int partsLength;
 
     public CheckoutList(int maxProducts) {
         this.maxProducts = maxProducts;
         this.arrayOfProducts = new String[maxProducts];
         this.arrayOfCosts = new Integer[maxProducts];
-        this.arrayOfCounts  = new Integer[maxProducts];
-        this.sortedProducts = new String[maxProducts];
+        this.arrayOfCounts = new Integer[maxProducts];
         this.count = 0;
-        this.s = "";
     }
 
     public String enterAndProcessCheckoutListData() {
@@ -30,11 +25,11 @@ public class CheckoutList {
         System.out.println("Enter Product Price Quantity, separated by spaces, up to " +
                 Integer.toString(maxProducts) + " different products, q to exit");
         while (count < maxProducts) {
-            s = sc.nextLine();
+            String s = sc.nextLine();
             if ("q".equals(s)) {
                 return s;
             }
-            addNewProductsToArrays();
+            addNewProductsToArrays(s);
         }
         sortCheckoutList();
         calcSumOfPurchases();
@@ -42,23 +37,22 @@ public class CheckoutList {
         return "q";
     }
 
-    private void addNewProductsToArrays () throws NumberFormatException {
-        splitUserLineByParts();
+    private void addNewProductsToArrays(String s) throws NumberFormatException {
+        String[] parts = splitUserLineByParts(s);
 
-        if (partsLength == 3) {
+        if (parts.length == 3) {
             String productName = parts[0];
-            Integer productCost = 0;
-            Integer productCount = 0;
+            int productCost = 0;
+            int productCount = 0;
             try {
                 productCost = Integer.parseInt(parts[1]);
                 productCount = Integer.parseInt(parts[2]);
-            }
-            catch (NumberFormatException ex) {
+            } catch (NumberFormatException ex) {
                 System.out.println("Wrong Number Format! Retry");
                 System.out.println(ex.getMessage());
             }
             boolean productAlreadyExists = false;
-            for (int i=0; i<=count; i++) {
+            for (int i = 0; i <= count; i++) {
                 if (productName.equals(arrayOfProducts[i])) {
                     arrayOfCosts[i] = productCost;
                     arrayOfCounts[i] += productCount;
@@ -74,48 +68,42 @@ public class CheckoutList {
         }
     }
 
-    private String[] splitUserLineByParts() {
-        parts = s.split(" ");
-        partsLength = parts.length;
-        if (partsLength != 3) {
+    private String[] splitUserLineByParts(String s) {
+        String[] parts = s.split(" ");
+        if (parts.length != 3) {
             System.out.println("Wrong number of arguments! Retry");
         }
         return parts;
     }
 
     private void sortCheckoutList() {
-        System.arraycopy(arrayOfProducts,0, sortedProducts, 0, maxProducts);
-        Arrays.sort(sortedProducts, (o1, o2) -> {
-            int res = String.CASE_INSENSITIVE_ORDER.compare(o1, o2);
-            if (res == 0) {
-                res = o1.compareTo(o2);
-            }
-            return res;
-        });
+        String[] sortedProducts = Arrays.copyOf(arrayOfProducts, maxProducts);
+        Comparator<String> comparator = String.CASE_INSENSITIVE_ORDER.thenComparing(Comparator.naturalOrder());
+        Arrays.sort(sortedProducts, comparator);
         System.out.println("Sorted Products:");
         System.out.println(Arrays.toString(sortedProducts));
     }
 
     private void calcSumOfPurchases() {
         long sum = 0;
-        for (int i = 0; i<count; i++) {
-            sum += arrayOfCosts[i] * arrayOfCounts[i];
+        for (int i = 0; i < count; i++) {
+            sum += (long) arrayOfCosts[i] * arrayOfCounts[i];
         }
         System.out.println("Sum of purchases: " + sum);
     }
 
-    private  void mostPopularProducts() {
-        for (int j = 0; j<3; j++) {
-            int indexOfMostPopolarProduct = 0;
-            for (int i = 0; i<count; i++) {
-                if (arrayOfCounts[i] > arrayOfCounts[indexOfMostPopolarProduct]) {
-                    indexOfMostPopolarProduct = i;
+    private void mostPopularProducts() {
+        for (int j = 0; j < 3; j++) {
+            int mostPopular = 0;
+            for (int i = 0; i < count; i++) {
+                if (arrayOfCounts[i] > arrayOfCounts[mostPopular]) {
+                    mostPopular = i;
                 }
             }
-            System.out.println("Most popular product is: " + arrayOfProducts[indexOfMostPopolarProduct]);
-            arrayOfProducts[indexOfMostPopolarProduct] = null;
-            arrayOfCounts[indexOfMostPopolarProduct] = 0;
-            arrayOfCosts[indexOfMostPopolarProduct] = 0;
+            System.out.println("Most popular product is: " + arrayOfProducts[mostPopular]);
+            arrayOfProducts[mostPopular] = null;
+            arrayOfCounts[mostPopular] = 0;
+            arrayOfCosts[mostPopular] = 0;
         }
     }
 }
