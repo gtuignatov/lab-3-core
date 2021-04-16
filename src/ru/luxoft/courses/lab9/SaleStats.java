b7dc43038ec068cddf94c0ce82136f4cf8d382a7
Aleksandr Ignatov <gtuignatov@yandex.ru>
2021-04-16 17:58:31 +0300

    Labs

--- src/ru/luxoft/courses/lab9/SaleStats.java
+++ src/ru/luxoft/courses/lab9/SaleStats.java
@@ -7,57 +7,47 @@ import java.util.TreeMap;
 public class SaleStats {
 
     TreeMap<String, TreeMap<String, Integer>> clients = new TreeMap<>();
-    private String userStatString = "";
-    private String[] parts;
-    private int partsLength;
-    private String clientName;
-    private String productName;
-    private Integer productCount;
 
     public String addSale() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter BuyerName, Product and Quantity, separated by spaces, exit command to cancel");
+        String userStatString;
         while (true) {
             userStatString = sc.nextLine();
             if ("exit".equals(userStatString)) {
                 return userStatString;
             }
-            checkUserData();
+            checkUserData(userStatString);
         }
     }
 
-    private void checkUserData() throws NumberFormatException {
-        resetUserData();
-        splitUserLineByParts();
-        if (partsLength == 3) {
+    private void checkUserData(String userStatString) throws NumberFormatException {
+        String clientName = "";
+        String productName = "";
+        Integer productCount = 0;
+        String[] parts = splitUserLineByParts(userStatString);
+        if (parts.length == 3) {
             clientName = parts[0];
             productName = parts[1];
             try {
                 productCount = Integer.parseInt(parts[2]);
+                addNewSaleToTreeMap(clientName, productName, productCount);
             } catch (NumberFormatException ex) {
                 System.out.println("Wrong Number Format! Retry");
                 System.out.println(ex.getMessage());
             }
-            addNewSaleToTreeMap();
         }
     }
 
-    private void resetUserData() {
-        clientName = "";
-        productName = "";
-        productCount = 0;
-    }
-
-    private String[] splitUserLineByParts() {
-        parts = userStatString.split(" ");
-        partsLength = parts.length;
-        if (partsLength != 3) {
+    private String[] splitUserLineByParts(String userStatString) {
+        String[] parts = userStatString.split(" ");
+        if (parts.length != 3) {
             System.out.println("Wrong number of arguments! Retry");
         }
         return parts;
     }
 
-    private void addNewSaleToTreeMap() {
+    private void addNewSaleToTreeMap(String clientName, String productName, Integer productCount) {
         clients.putIfAbsent(clientName, new TreeMap<>());
         TreeMap<String, Integer> temp = clients.get(clientName);
         temp.putIfAbsent(productName, 0);
@@ -77,8 +67,6 @@ public class SaleStats {
             }
         }
     }
-
-
 }
 
 
